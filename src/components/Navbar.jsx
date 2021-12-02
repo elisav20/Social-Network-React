10a049e04e8ebf022618307dc015f9a6e9144be0
Vasile Cristel <vasilec@usalinksystem.com>
2021-12-02 23:16:12 +0200

    add header and sidebar and connect sass module

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div className="nav__item">
+                <a className="nav__link" href="#">Profile</a>
+            </div>
+            <div className="nav__item">
+                <a className="nav__link" href="#">Messages</a>
+            </div>
+            <div className="nav__item">
+                <a className="nav__link" href="#">News</a>
+            </div>
+            <div className="nav__item">
+                <a className="nav__link" href="#">Music</a>
+            </div>
+            <div className="nav__item">
+                <a className="nav__link" href="#">Settings</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
