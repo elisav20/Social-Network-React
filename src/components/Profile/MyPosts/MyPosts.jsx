23a918981f54689ab04686e74973a4a3d93330f2
Vasile Cristel <vasilec@usalinksystem.com>
2021-12-17 13:45:32 +0200

    add callback for add post button

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,13 +5,21 @@ const MyPosts = (props) => {
     const postsData = props.postsData.map((post) => (
         <Post message={post.message} likesCount={post.likesCount} />
     ));
+    const newPostElement = React.createRef();
+
+    const addPost = () => {
+        const text = newPostElement.current.value;
+        alert(text);
+    };
 
     return (
         <div className="my-posts">
             <h3>My posts</h3>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea ref={newPostElement}></textarea>
+                </div>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className="posts">{postsData}</div>
         </div>
