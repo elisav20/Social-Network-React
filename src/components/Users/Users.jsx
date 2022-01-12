64166719d31532535b1114d94e85df941f4b8666
Vasile Cristel <vasilec@usalinksystem.com>
2022-01-12 13:54:14 +0200

    styling the page with users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,31 +1,89 @@
 import React from "react";
 
 const Users = (props) => {
+    if (props.users.lengt === 0) {
+        props.setUsers([
+            {
+                id: 1,
+                photoUrl:
+                    "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
+                fullName: "Dmitry",
+                status: "I am a boss",
+                location: {
+                    city: "Minsk",
+                    country: "Belarus",
+                },
+                followed: true,
+            },
+            {
+                id: 2,
+                fullName: "Sasha",
+                photoUrl:
+                    "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
+                status: "I am a boss too",
+                location: {
+                    city: "Russia",
+                    country: "Moscow",
+                },
+                followed: false,
+            },
+            {
+                id: 3,
+                fullName: "Andrew",
+                photoUrl:
+                    "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
+                status: "I am a boss too",
+                location: {
+                    city: "Ukraine",
+                    country: "Kiev",
+                },
+                followed: true,
+            },
+        ]);
+    }
+
     return (
         <div>
             {props.users.map((user) => (
-                <div key={user.id}>
-                    <span>
+                <div className="user__item" key={user.id}>
+                    <div>
                         <div>
-                            <img src={user.photoUrl} alt={user.fullName} />
+                            <img
+                                className="user__img"
+                                src={user.photoUrl}
+                                alt={user.fullName}
+                            />
                         </div>
                         <div>
-                            <button>Follow</button>
+                            {user.followed ? (
+                                <button
+                                    className="user__btn-follow"
+                                    onClick={() => props.unfollow(user.id)}
+                                >
+                                    Unfollow
+                                </button>
+                            ) : (
+                                <button
+                                    className="user__btn-follow"
+                                    onClick={() => props.follow(user.id)}
+                                >
+                                    Follow
+                                </button>
+                            )}
                         </div>
-                    </span>
-                    <span>
-                        <span>
+                    </div>
+                    <div className="user__info">
+                        <div>
                             <div>{user.fullName}</div>
                             <div>{user.status}</div>
-                        </span>
-                        <span>
+                        </div>
+                        <div>
                             <div>{user.location.country}</div>
                             <div>{user.location.city}</div>
-                        </span>
-                    </span>
+                        </div>
+                    </div>
                 </div>
             ))}
-            )
         </div>
     );
 };
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,41 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
 const initialState = {
-    users: [
-        {
-            id: 1,
-            photoUrl: "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
-            fullName: "Dmitry",
-            status: "I am a boss",
-            location: {
-                city: "Minsk",
-                country: "Belarus",
-            },
-            followed: true,
-        },
-        {
-            id: 2,
-            fullName: "Sasha",
-            photoUrl: "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
-            status: "I am a boss too",
-            location: {
-                city: "Russia",
-                country: "Moscow",
-            },
-            followed: false,
-        },
-        {
-            id: 3,
-            fullName: "Andrew",
-            photoUrl: "https://i.pinimg.com/474x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
-            status: "I am a boss too",
-            location: {
-                city: "Ukraine",
-                country: "Kiev",
-            },
-            followed: true,
-        },
-    ], 
+    users: [], 
 };
 
 const usersReducer = (state = initialState, action) => {
