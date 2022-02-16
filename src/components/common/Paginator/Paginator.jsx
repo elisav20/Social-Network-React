61d4cdd3453ebd3fbdff83e6ff35ded897c29007
Vasile Cristel <vasilec@usalinksystem.com>
2022-02-16 19:47:02 +0200

    change paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Paginator = ({ totalCount, pageSize, currentPage, onPageChanged }) => {
+const Paginator = ({
+    totalCount,
+    pageSize,
+    currentPage,
+    onPageChanged,
+    portionSize = 10,
+}) => {
     const pagesCount = Math.ceil(totalCount / pageSize);
     const pages = [];
 
     for (let i = 1; i <= pagesCount; i++) {
-        if (pages.length < 10) {
-            pages.push(i);
-        }
+        pages.push(i);
     }
+
+    const portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
 
     return (
         <div className="pagination">
-            {pages.map((page) => {
-                return (
-                    <button
-                        className={
-                            page === currentPage
-                                ? "pagination__item active"
-                                : "pagination__item"
-                        }
-                        onClick={() => {
-                            onPageChanged(page);
-                        }}
-                    >
-                        {page}
-                    </button>
-                );
-            })}
+            {portionNumber > 1 && (
+                <button onClick={() => setPortionNumber(portionNumber - 1)}>
+                    PREV
+                </button>
+            )}
+
+            {pages
+                .filter(
+                    (page) =>
+                        page >= leftPortionPageNumber &&
+                        page <= rightPortionPageNumber
+                )
+                .map((page) => {
+                    return (
+                        <button
+                            className={
+                                page === currentPage
+                                    ? "pagination__item active"
+                                    : "pagination__item"
+                            }
+                            key={page}
+                            onClick={() => {
+                                onPageChanged(page);
+                            }}
+                        >
+                            {page}
+                        </button>
+                    );
+                })}
+
+            {portionCount > portionNumber && (
+                <button onClick={() => setPortionNumber(portionNumber + 1)}>
+                    NEXT
+                </button>
+            )}
         </div>
     );
 };
